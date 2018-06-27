8025b5337b3cbf0d28a287b30ed03e55411abaa5
Janusz Marcinik <januszmarcinik@gmail.com>
2018-06-27 19:37:03 +0200

    Add autofac and divide services configuration

---
+++ Exams/EduNurse.Exams.Api/Configuration/ContainerConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using AutoMapper;
+using EduNurse.Exams.Api.Repositories;
+using EduNurse.Exams.Shared.Repositories;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EduNurse.Exams.Api.Configuration
+{
+    internal static class ContainerConfiguration
+    {
+        public static IContainer ConfigureContainer(this IServiceCollection services)
+        {
+            var builder = new ContainerBuilder();
+            builder.Populate(services);
+
+            builder.RegisterType<ExamsContext>().As<IExamsContext>().InstancePerLifetimeScope();
+            builder.RegisterType<QuestionsRepository>().As<IQuestionsRepository>().InstancePerLifetimeScope();
+            builder.RegisterType<ExamsRepository>().As<IExamsRepository>().InstancePerLifetimeScope();
+            builder.RegisterInstance(AutoMapperConfig.Initialize()).As<IMapper>().SingleInstance();
+
+            return builder.Build();
+        }
+
+        public static void ConfigureContainer(this IApplicationLifetime appLifetime, IContainer container)
+        {
+            appLifetime.ApplicationStopped.Register(container.Dispose);
+        }
+    }
+}
---
+++ Exams/EduNurse.Exams.Api/Configuration/DatabaseConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EduNurse.Exams.Api.Configuration
+{
+    internal static class DatabaseConfiguration
+    {
+        public static void ConfigureDatabase(this IServiceCollection services)
+        {
+            services.AddDbContext<ExamsContext>(options =>
+            {
+                options.UseInMemoryDatabase("Testing");
+            });
+        }
+    }
+}
